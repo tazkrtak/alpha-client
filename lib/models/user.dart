4839f28a8f0357104ebfcff0e4386f3b1f9ce5a1
MichelineMedhat <michelinemedhat@gmail.com>
2019-10-08 19:59:20 -0700

    Add Signing In/Up with Firebase

---
+++ lib/models/user.dart
@@ -0,0 +1,15 @@
+class User {
+  String name;
+  String email;
+  String nationalId;
+  String phoneNumber;
+  String password;
+  double balance;
+  String sharedKey;
+
+  static final User _user = User._internal();
+  factory User() {
+    return _user;
+  }
+  User._internal();
+}
